9dac229575812388d48e511ef8db5f01aff4e2af
dinasai <138925273+dinasai@users.noreply.github.com>
2023-08-11 13:26:46 -0700

    7th task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * description: number positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

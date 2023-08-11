6ed05502b872f808c2b5996a25df6371fc3a9b46
dinasai <138925273+dinasai@users.noreply.github.com>
2023-08-11 12:12:48 -0700

    2nd task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * description: number positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

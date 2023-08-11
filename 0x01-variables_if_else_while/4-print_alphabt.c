f7d2105ca9862af7abc21e87a07b7fb6051debfd
dinasai <138925273+dinasai@users.noreply.github.com>
2023-08-11 13:03:57 -0700

    4th task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * description: number positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}

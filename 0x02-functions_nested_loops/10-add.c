0de5e126e92cc50a0b4e924d3781bf7809126fdd
dinasai <138925273+dinasai@users.noreply.github.com>
2023-08-16 17:40:29 -0700

    10th task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
-/*
- * add - function that adds two integers
+/**
+ * add - function that sums two numbers
  * @a: para 1
  * @b: para 2
  * Return: a + b
  */
+
 int add(int a, int b)
 {
 	return (a + b);
